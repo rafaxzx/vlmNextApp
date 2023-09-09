ca3ec823d35e21a99579def8c5b4ef6bfb7f01e4
Rafael <rafa_xzx@hotmail.com>
2023-09-09 10:43:19 -0300

    New colors, new icons  and new layout for screen Search Items

--- src/app/VLM/page.jsx
+++ src/app/VLM/page.jsx
@@ -1,51 +1,47 @@
-import Link from "next/link";
 import "../../app/globals.css";
+import Link from "next/link";
 import ContentArea from "@/components/ContentArea/ContentArea";
+import { FaMagnifyingGlass, FaPenToSquare } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <ContentArea>
       <div className=" w-full flex flex-col gap-y-8 bg-gray-500 items-center p-4 border-b-gray-400 border-2">
-        <p>Pesquisar</p>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Itens
-        </Link>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Fabricantes
-        </Link>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Setores/Equipamentos
-        </Link>
-      </div>
-      <div className=" w-full flex flex-col gap-y-8 bg-gray-500 items-center p-4 border-b-gray-400 border-2">
-        <p>Cadastrar - Editar</p>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Itens
-        </Link>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Fabricantes
-        </Link>
-        <Link
-          href={"#"}
-          className="bg-slate-300 text-black shadow-lg p-2 rounded-lg"
-        >
-          Setores/Equipamentos
-        </Link>
+        <h1 className="text-2xl">Gerenciar</h1>
+
+        <div>
+          <div className="flex items-center gap-x-3 bg-gray-100 text-black shadow-lg p-3 rounded-lg">
+            <p className="text-xl ">Item</p>
+            <Link href={"/SearchItem"}>
+              <FaMagnifyingGlass size={"1.5em"}></FaMagnifyingGlass>
+            </Link>
+            <Link href={"#"}>
+              <FaPenToSquare size={"1.5em"}></FaPenToSquare>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-x-3 bg-gray-100 text-black shadow-lg p-3 rounded-lg">
+            <p className="text-xl ">Fabricante</p>
+            <Link href={"/#"}>
+              <FaMagnifyingGlass size={"1.5em"}></FaMagnifyingGlass>
+            </Link>
+            <Link href={"#"}>
+              <FaPenToSquare size={"1.5em"}></FaPenToSquare>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-x-3 bg-gray-100 text-black shadow-lg p-3 rounded-lg">
+            <p className="text-xl ">Setor/Equipamento</p>
+            <Link href={"/#"}>
+              <FaMagnifyingGlass size={"1.5em"}></FaMagnifyingGlass>
+            </Link>
+            <Link href={"#"}>
+              <FaPenToSquare size={"1.5em"}></FaPenToSquare>
+            </Link>
+          </div>
+        </div>
       </div>
     </ContentArea>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,19 +9,20 @@ export default function Footer() {
       fixed
       bottom-0
       border-t
-    bg-blue-400
+    bg-gray-600
     flex justify-center
     items-center
     p-2
     gap-2
     gap-x-4
     m-auto
-    text-xl"
+    text-sm
+    md:text-lg"
     >
-      <Link href={"#"} className="border rounded p-2 hover:bg-blue-800">
+      <Link href={"#"} className="border rounded p-2 hover:bg-gray-800">
         Contato
       </Link>
-      <Link href={"#"} className="border rounded p-2 hover:bg-blue-800">
+      <Link href={"#"} className="border rounded p-2 hover:bg-gray-800">
         Manut 721
       </Link>
     </footer>
