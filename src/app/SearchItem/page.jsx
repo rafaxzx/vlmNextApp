8853f5aeed8f3457b9095735a40c813fd70642b7
rafaxzx <rafa_xzx@hotmail.com>
2023-09-24 15:46:11 -0300

    New layout for search Item page, more clean

--- src/app/SearchItem/page.jsx
+++ src/app/SearchItem/page.jsx
@@ -8,46 +8,18 @@ export default function SearchItem() {
     <ContentArea>
       <div className="mb-12 py-8 w-full h-auto flex flex-col gap-y-8 bg-gray-500 items-center p-4 border-b-gray-400 border-2">
         <h1 className="text-xl">Pesquisar Item por atributo:</h1>
-        <div className="bg-gray-100 text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4">
-          Nome
-          <input type="radio" id="nome" name="searchType" value={"name"} />
-        </div>
-        <div className="bg-gray-100 text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4">
-          Especificação
-          <input
-            type="radio"
-            id="specification"
-            name="searchType"
-            value={"especification"}
-          />
-        </div>
-        <div className="bg-gray-100 text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4">
-          Fabricante
-          <input
-            type="radio"
-            id="manufacturer"
-            name="searchType"
-            value={"manufacturer"}
-          />
-        </div>
-        <div className="bg-gray-100 text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4">
-          Setor/Equipamento
-          <input
-            type="radio"
-            id="sectorEquipament"
-            name="searchType"
-            value={"sectorEquip"}
-          />
-        </div>
-        <div className="bg-gray-100 text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4">
-          Código interno
-          <input
-            type="radio"
-            id="internCode"
-            name="searchType"
-            value={"internCode"}
-          />
-        </div>
+        <select
+          className="bg-gray-200 text-xl text-black shadow-lg p-2 rounded-lg flex items-center gap-x-4"
+          name="searchOptions"
+          id="searchOptions"
+        >
+          <option value="name">Nome</option>
+          <option value="specification">Especificação</option>
+          <option value="manufacturer">Fabricante</option>
+          <option value="internCode">Código Interno</option>
+          <option value="sectorEquipament">Setor/Equipamento</option>
+        </select>
+
         <div className="flex items-center gap-x-4">
           <input
             className="rounded-lg bg-gray-200 p-2 text-xl text-black"
@@ -60,7 +32,10 @@ export default function SearchItem() {
             <FaMagnifyingGlass size={"1.5em"} />
           </Link>
         </div>
-        <h1 className="text-xl">Pesquisar Item com estoque:</h1>
+
+        <hr className="w-80 lg:w-96 bg-gray-400 border-0 h-px"></hr>
+
+        <h1 className="text-xl">Listar Itens com estoque:</h1>
         <div className="flex gap-x-4">
           <Link
             href={"#"}
@@ -75,6 +50,7 @@ export default function SearchItem() {
             Zerado
           </Link>
         </div>
+        <hr className="w-80 lg:w-96 bg-gray-400 border-0 h-px"></hr>
       </div>
     </ContentArea>
   );
