126548d5c0f722bc685b832336e9ee2b17b618ba
Rafael <rafa_xzx@hotmail.com>
2023-11-08 20:54:27 -0300

    Change to const the nameReq variable

--- src/app/api/manufacturer/byName/[name]/route.js
+++ src/app/api/manufacturer/byName/[name]/route.js
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-  let nameReq = params.name;
+  const nameReq = params.name;
   const manufacturerByName = await prisma.manufacturer.findMany({
     where: { name: { contains: nameReq } },
   });
