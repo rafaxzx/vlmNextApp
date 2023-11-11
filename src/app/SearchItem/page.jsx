4ee306765b4f292bf5159a9559b49cfd180f51cb
Rafael <rafa_xzx@hotmail.com>
2023-11-11 18:22:40 -0300

    Removed an import

--- src/app/SearchItem/page.jsx
+++ src/app/SearchItem/page.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import ContentArea from "@/components/ContentArea/ContentArea";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import ItemCard from "@/components/ItemCard/ItemCard";
-import { prisma } from "@/lib/prisma";
 
 export default function SearchItem() {
   return (
